d52219f8ff910582ffb3e210af038da116675ae3
Istvan Hornyak <hornyak.istvan25@gmail.com>
2022-12-14 21:46:33 +0100

    added sixth day's solutions

---
+++ day6/solution_part1.py
@@ -0,0 +1,9 @@
+distinct_chars = 4
+
+with open("input.txt") as buffer:
+    data = buffer.read()
+    for i in range(len(data)):
+        start_index, end_index = 0 + i, distinct_chars + i
+        if len(set(data[start_index: end_index])) == distinct_chars:
+            print(i  + distinct_chars)
+            break
